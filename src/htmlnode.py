5cc252ac6cef91a8f5e69488739602b40cfce308
nogilvie02 <nogilvie02@gmail.com>
2026-02-04 09:45:32 -0800

    Added LeafNodes

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -18,3 +18,17 @@ class HTMLNode():
 
     def __repr__(self):
         return f'HTMLNode("{self.tag}", "{self.value}", {self.children}, {self.props_to_html()})'
+    
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, None, props)
+
+    def to_html(self):
+        if self.value == "" or self.value == None:
+            raise ValueError("All LeafNodes need a value")
+        if self.tag == None:
+            return self.value
+        return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+    
+    def __repr__(self):
+        return f'LeafNode("{self.tag}", "{self.value}", {super().props_to_html()})'
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 
 class TestHTMLNode(unittest.TestCase):
@@ -53,6 +53,27 @@ class TestHTMLNode(unittest.TestCase):
     def test_to_html_not_implemented(self):
         node = HTMLNode()
         with self.assertRaises(NotImplementedError):
+            node.to_html()
+
+    def test_leaf_to_html_p(self):
+        node = LeafNode("p", "Hello, world!")
+        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+
+    def test_leaf_to_html_a(self):
+        node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+        self.assertEqual(node.to_html(), '<a href="https://www.google.com">Click me!</a>')
+
+    def test_leaf_to_html_b(self):
+        node = LeafNode("b", "This text is bold!")
+        self.assertEqual(node.to_html(), "<b>This text is bold!</b>")
+
+    def test_leaf_to_html_no_tag(self):
+        node = LeafNode(None, "This text is raw!")
+        self.assertEqual(node.to_html(), "This text is raw!")
+
+    def test_leaf_to_html_no_value(self):
+        node = LeafNode("p", None)
+        with self.assertRaises(ValueError):
             node.to_html()
 
 if __name__ == "__main__":
